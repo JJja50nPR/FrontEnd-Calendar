ca7c336235922a5cb87ca45b140d6f827b34c49e
Jason <jasonpalacios1346@gmail.com>
2023-06-15 16:08:03 -0600

    Cambios env

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -4,7 +4,7 @@ export const getEnvVariables = () => {
     // import.meta.env;
 
     return {
-        VITE_API_URL: import.meta.env.VITE_API_URLRAILWAY,
+        VITE_API_URL: import.meta.env.VITE_API_URL,
     }
   
 }
